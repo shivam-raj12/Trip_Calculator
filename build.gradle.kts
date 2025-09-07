e3381219ddb601a83082adac0c1f99adee423a33
Shivam <157960855+shivam-raj12@users.noreply.github.com>
2025-09-07 21:33:40 +0530

    Upgrade: Update Android SDK and library versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,5 +5,5 @@ plugins {
     alias(libs.plugins.kotlin.compose) apply false
 
 
-    id("com.google.devtools.ksp") version "2.0.21-1.0.28" apply false
+    id("com.google.devtools.ksp") version "2.2.10-2.0.2" apply false
 }
